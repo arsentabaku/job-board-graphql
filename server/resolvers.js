74954f0b6b5a68ad5cd9898f4ceeccb9f5586c23
Arsen Tabaku <arsen.tabaku@vrdirect.com>
2024-01-18 12:22:17 +0100

    Enhanced schema and replaced mock data of job resolver with real data comming from database

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,5 +1,15 @@
+import { getJobs } from "./db/jobs.js";
+
 export const resolvers = {
   Query: {
-    greeting: () => "Hello world!",
+    job: async () => getJobs(),
+  },
+
+  Job: {
+    date: (job) => toIsoDate(job.createdAt),
   },
 };
+
+function toIsoDate(value) {
+  return value.slice(0, "yyyy-mm-dd".length);
+}
