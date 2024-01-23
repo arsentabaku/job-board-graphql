56331d5ce2f2496f7b768bce1c55576ce1c27291
Arsen Tabaku <arsen.tabaku@vrdirect.com>
2024-01-23 10:42:50 +0100

    Implemented get company by company id

--- client/src/pages/CompanyPage.js
+++ client/src/pages/CompanyPage.js
@@ -1,18 +1,23 @@
-import { useParams } from 'react-router';
-import { companies } from '../lib/fake-data';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { getCompany } from "../lib/graphql/queries";
 
 function CompanyPage() {
   const { companyId } = useParams();
+  const [company, setCompany] = useState();
+  useEffect(() => {
+    getCompany(companyId).then(setCompany);
+  }, [companyId]);
 
-  const company = companies.find((company) => company.id === companyId);
+  console.log("[CompanyPage] company:", company);
+  if (!company) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
-      <h1 className="title">
-        {company.name}
-      </h1>
-      <div className="box">
-        {company.description}
-      </div>
+      <h1 className="title">{company.name}</h1>
+      <div className="box">{company.description}</div>
     </div>
   );
 }
